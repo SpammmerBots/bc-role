835e08bfc3c3268e84726091c33ceea7a470caab
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-11-30 14:44:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -753,7 +753,7 @@ client.on('guildMemberAdd', member => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "chat");
+    const stewart = member.guild.channels.find("name", "☆-「chat");
      stewart.send(`<@${member.user.id}>**Invite By**:<@${inviter.id}>:champagne_glass:`);
    //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
